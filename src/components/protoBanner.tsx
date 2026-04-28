a70c82d95e8244c8af6b8811590219eeed346e98
Donald Labaj <donald.labaj@gmail.com>
2026-04-28 08:50:25 -0400

    chore: removed default from proto banner.

--- src/components/protoBanner.tsx
+++ src/components/protoBanner.tsx
@@ -3,7 +3,7 @@ import { Banner, Bullseye } from "@patternfly/react-core";
 
 const ProtoBanner: React.FC = () => {
   return (
-    <Banner default isSticky>
+    <Banner isSticky>
       <Bullseye>
         <strong>This application is a design prototype</strong>
       </Bullseye>
